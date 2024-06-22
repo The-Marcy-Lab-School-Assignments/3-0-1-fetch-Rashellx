65c92a6b3b3f27bf252f7f85a3fdb4d938ab8ed9
Rashell Aldas <rashellaldas@rashells-mbp.mynetworksettings.com>
2024-06-21 23:24:25 -0400

    wow

--- src/fetch-functions.js
+++ src/fetch-functions.js
@@ -1,13 +1,29 @@
-const userUrl = 'https://jsonplaceholder.typicode.com/users'
+const userUrl = 'https://jsonplaceholder.typicode.com/users';
 
 export const checkResponseStatus = () => {
+
+  return fetch(userUrl).then(({ status, ok, url }) => ({ status, ok, url }))
+    ;
 };
 
 export const getUsers = () => {
+  return fetch(userUrl).then((userArr) => userArr.json())
 };
 
-export const getUserPosts = () => {
+export const getUserPosts = (userId, maxNumberPosts = 3) => {
+  const userPosts = `https://jsonplaceholder.typicode.com/users/${userId}/posts`
+  return fetch(userPosts)
+    .then((postsArr) => postsArr.json())
+    .then((dataArr) => dataArr.splice(0, maxNumberPosts))
 };
 
-export const createNewUser = () => {
+export const createNewUser = (newUserData) => {
+  const postNewUser = {
+    method: "POST",                      // The type of HTTP request (type of method)
+    body: JSON.stringify(newUserData),       // The data to be sent to the API ()
+    headers: {
+      "Content-Type": "application/json" // The format of the body's data (what type of content it is)
+    }
+  }
+  return fetch(userUrl, postNewUser).then((newUserObj) => newUserObj.json())
 }
